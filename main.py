6c93ce15ac89d22be26e8094a8d6f870987c8708
helioole <irarakovcena@gmail.com>
2023-04-18 14:26:11 +0300

    added unit tests

--- main.py
+++ main.py
@@ -1,14 +1,72 @@
 from grammar.ChomskyNormalForm import Chomsky
 
-chomsky = Chomsky('S',
-['S', 'A', 'B', 'C'],
-['a', 'd'],
-{
-'S': ["dB", "A"],
-'A': ["d", "dS", "aBdAB"],
-'B': ["a", "dA", "A", "ε"],
-'C': ["Aa"]
-})
+def main():
+    chomsky = Chomsky('S',
+    ['S', 'A', 'B', 'C'],
+    ['a', 'd'],
+    {
+    'S': ["dB", "A"],
+    'A': ["d", "dS", "aBdAB"],
+    'B': ["a", "dA", "A", "ε"],
+    'C': ["Aa"]
+    })
 
-chomsky.cfg_to_cnf()
-chomsky.print_grammar()
+    # chomsky = Chomsky('S',
+    #                   ['S', 'A', 'B', 'C', 'D'],
+    #                   ['a', 'b'],
+    #                   {
+    #                       'S': ["a", "aD", "aA", "B"],
+    #                       'A': ["aBB", "ε"],
+    #                       'D': ["aD"],
+    #                       'C': ["aC"],
+    #                       'B': ["Aa", "b"]
+    #                   })
+
+    chomsky1 = Chomsky('S',
+    ['S', 'A', 'B', 'C', 'E'],
+    ['a', 'b'],
+    {
+    'S': ["aB", "AC"],
+    'A': ["a", "ASC", "BC"],
+    'B': ["b", "bS"],
+    'C': ["ε", "BA"],
+    'E': ["bB"]
+    })
+
+    chomsky2 = Chomsky('S',
+    ['S', 'A', 'B', 'C', 'D'],
+    ['a', 'd'],
+    {
+    'S': ["bA", "AC"],
+    'A': ["bS", "BC", "AbAa"],
+    'B': ["BbaA", "a", "bSa"],
+    'C': ["ε"],
+    'D': ["AB"]
+    })
+
+    # chomsky.eliminate_epsilon_productions()
+    # print(chomsky.productions)
+    # print(chomsky.nonTerminal)
+
+    # print(chomsky.productions)
+    # print(chomsky.nonTerminal)
+    #
+    chomsky.eliminate_epsilon_productions()
+    print(chomsky.productions)
+    print(chomsky.nonTerminal)
+    chomsky.eliminate_unit_productions()
+    print(chomsky.productions)
+    print(chomsky.nonTerminal)
+    chomsky.eliminate_inaccessible_symbols()
+    print(chomsky.productions)
+    print(chomsky.nonTerminal)
+    chomsky.eliminate_nonproductive()
+    print(chomsky.productions)
+    print(chomsky.nonTerminal)
+
+
+
+    # chomsky.cfg_to_cnf()
+    # chomsky.print_grammar()
+
+main()
---
+++ unittests/tests.py
@@ -0,0 +1,99 @@
+import unittest
+from grammar.ChomskyNormalForm import Chomsky
+
+
+
+class TestConverter(unittest.TestCase):
+
+    def test_remove_epsilon_productions(self):
+        chomsky = Chomsky('S',
+                          ['S', 'A', 'B', 'C'],
+                          ['a', 'd'],
+                          {
+                              'S': ["dB", "A"],
+                              'A': ["d", "dS", "aBdAB"],
+                              'B': ["a", "dA", "A", "ε"],
+                              'C': ["Aa"]
+                          })
+
+        chomsky.eliminate_epsilon_productions()
+        new_productions = chomsky.productions
+        new_nonTerminals = chomsky.nonTerminal
+        expected_productions = {'S': ['d', 'dB', 'A'],
+                                'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'B': ['a', 'dA', 'A'],
+                                'C': ['Aa']}
+
+        expected_terminals = ['S', 'A', 'B', 'C']
+
+        self.assertCountEqual(new_productions, expected_productions)
+        self.assertCountEqual(new_nonTerminals, expected_terminals)
+
+    def test_remove_unit_productions(self):
+        chomsky = Chomsky('S',
+                          ['S', 'A', 'B', 'C'],
+                          ['a', 'd'],
+                          {'S': ['d', 'dB', 'A'],
+                           'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                           'B': ['a', 'dA', 'A'],
+                           'C': ['Aa']}
+                          )
+
+        chomsky.eliminate_unit_productions()
+        new_productions = chomsky.productions
+        new_nonTerminals = chomsky.nonTerminal
+        expected_productions = {'S': ['d', 'dB', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'B': ['a', 'dA', 'd', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'C': ['Aa']}
+
+        expected_terminals = ['S', 'A', 'B', 'C']
+
+        self.assertCountEqual(new_productions, expected_productions)
+        self.assertCountEqual(new_nonTerminals, expected_terminals)
+
+    def test_inaccessible_symbols(self):
+        chomsky = Chomsky('S',
+                          ['S', 'A', 'B', 'C'],
+                          ['a', 'd'],
+                          {'S': ['d', 'dB', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                           'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                           'B': ['a', 'dA', 'd', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                           'C': ['Aa']}
+                          )
+
+        chomsky.eliminate_inaccessible_symbols()
+        new_productions = chomsky.productions
+        new_nonTerminals = chomsky.nonTerminal
+        expected_productions = {'S': ['d', 'dB', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'B': ['a', 'dA', 'd', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA']}
+
+        expected_terminals = ['S', 'A', 'B']
+
+        self.assertCountEqual(new_productions, expected_productions)
+        self.assertCountEqual(new_nonTerminals, expected_terminals)
+
+    def test_remove_nonproductive_symbols(self):
+        chomsky = Chomsky('S',
+                          ['S', 'A', 'B', 'C'],
+                          ['a', 'd'],
+                          {'S': ['d', 'dB', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                           'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                           'B': ['a', 'dA', 'd', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA']}
+                          )
+
+        chomsky.eliminate_inaccessible_symbols()
+        new_productions = chomsky.productions
+        new_nonTerminals = chomsky.nonTerminal
+        expected_productions = {'S': ['d', 'dB', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'A': ['d', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA'],
+                                'B': ['a', 'dA', 'd', 'dS', 'adA', 'aBdAB', 'adAB', 'aBdA']}
+
+        expected_terminals = ['A', 'B', 'S']
+
+        self.assertCountEqual(new_productions, expected_productions)
+        self.assertCountEqual(new_nonTerminals, expected_terminals)
+
+if __name__ == '__unittest__':
+    unittest.main()
